d80a5caeffbfbd04c0c0fdbddaaf45e2fe55c09d
micahrobinsondesigns <micahrobinsondesigns@gmail.com>
2019-05-31 14:20:45 -0700

    Remove spacing

--- woocommerce-tag-chips.php
+++ woocommerce-tag-chips.php
@@ -3,11 +3,9 @@
 /**
  * @wordpress-plugin
  * Plugin Name:       Woocommerce Tag Chips
- * Plugin URI:        https://bedroomsandmore.com
  * Description:       Add form fields to Edit Tag page in wp-admin
  * Version:           1.0
  * Author:            Micah Robinson
- * Author URI:        https://bedroomsandmore.com
  * License:           GPL-2.0
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       woocommerce-tag-chips
@@ -19,15 +17,13 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 		// ONLY RUN IF WOOCOMMERCE IS ACTIVE....
-		
+
 	if ( is_admin() ) {
 		$taxonomy= 'product_tag';
-	
-		add_action( "{$taxonomy}_add_form_fields", 'add_chip_tag_form', 10); 
-		add_action( "{$taxonomy}_edit_form_fields", 'edit_chip_tag_form', 10, 2 ); 
+		add_action( "{$taxonomy}_add_form_fields", 'add_chip_tag_form', 10);
+		add_action( "{$taxonomy}_edit_form_fields", 'edit_chip_tag_form', 10, 2 );
 		function add_chip_tag_form( $taxonomy ) {
 			?>
 				<div class="form-field">
@@ -55,7 +51,6 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 			$termid = $tag->term_id;
 			$chip_text_str = get_option( "chip_$termid" );
 			$chip_text_obj = unserialize( $chip_text_str );
-			
 			?>
 		    <tr class="form-field">
 		        <th valign="top" scope="row">
@@ -95,50 +90,33 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 				</tr>
 			<?php
 		};
-		
-		
+
 		add_action ( "edited_{$taxonomy}", 'save_chip_tag_form' );
 		add_action ( "created_{$taxonomy}",'save_chip_tag_form' );
-		// save extra category extra fields callback function
-		
 		function save_chip_tag_form( $term_id ) {
 			if ( isset( $_POST['chiptext'] ) ) {
-				
 				$termid= $term_id;
 				$prev_chip_text_str = get_option( "chip_$termid" );
-
 				if ( $prev_chip_text_str !== false ) {
-
 					$tag_chip['chip_text']= $_POST['chiptext'];
 					$tag_chip['chip_bg_color']= $_POST['chipbgcolor'];
 					$tag_chip['chip_txt_color']= $_POST['chiptxtcolor'];
-					
 					update_option( "chip_$termid", serialize($tag_chip) );
-					
 				} else {
-					
 					$tag_chip= array(
 						'chip_text'=> '',
 						'chip_bg_color'=> '',
 						'chip_txt_color'=> ''
 					);
-					
 					$tag_chip['chip_text']= $_POST['chiptext'];
 					$tag_chip['chip_bg_color']= $_POST['chipbgcolor'];
 					$tag_chip['chip_txt_color']= $_POST['chiptxtcolor'];
-					
 					add_option( "chip_$termid", serialize($tag_chip), '', 'yes' );
-
 				}
 			}
-		}  
-	}
-	
-	
-	
-	
+		}
+	};
 	add_action( 'woocommerce_single_product_summary', 'add_tag_chip', 4 );
-
 	function add_tag_chip(){
 		if ( is_product() ) {
 			global $product;
@@ -156,30 +134,18 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 				}
 			}
 		}
-	}
-	
-	
-	
-	
-	
+	};
+
 	// Register admin style sheet.
 	add_action( 'admin_enqueue_scripts', 'register_admin_tag_chips_styles' );
 	function register_admin_tag_chips_styles() {
 		wp_enqueue_style( 'woocommerce-tag-chips-admin-style', plugins_url('/woocommerce-tag-chips/admin/css/' ) . 'woocommerce-tag-chips.css' );
 		wp_enqueue_script('woocommerce-tag-chips-admin-script', plugins_url('/woocommerce-tag-chips/admin/js/' ) . 'woocommerce-tag-chips.js' );
-	}
+	};
 	// Register public style sheet.
 	add_action( 'wp_enqueue_scripts', 'register_public_tag_chips_styles' );
 	function register_public_tag_chips_styles() {
 		wp_register_style( 'woocommerce-tag-chips-public-style', plugins_url('/woocommerce-tag-chips/public/css/' ) . 'woocommerce-tag-chips.css' );
 		wp_enqueue_style('woocommerce-tag-chips-public-style');
-	}
-
+	};
 }
-
-
-
-// https://www.thecreativedev.com/how-to-add-extra-field-in-custom-taxonomy-wordpress/
-
-
-
